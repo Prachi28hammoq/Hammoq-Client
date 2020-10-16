927423854bbe0fed57648a65dd5eca5de740fa1f
MohitSinghTomar27 <mtmohit59@gmail.com>
2020-10-16 13:36:08 +0530

    changes

--- src/Pages/Messages/message.js
+++ src/Pages/Messages/message.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React , {useState, useEffect}from "react";
 import $ from "jquery";
 //import "./chat.css";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
@@ -8,54 +8,27 @@ const io = require("socket.io-client");
 const socket = io.connect("http://localhost:8000");
 
 
+const  Message = () => {
+  const [agents , setAgents] = useState([])
 
-export default class extends Component {
-  constructor(){
-    super()
-    this.state = {
-      agents : [],
-      clientId : localStorage.getItem('cid')
-    }
-  }
+  useEffect(() => {
+    loadAgent();
+  },[])
 
-  componentDidMount = async () => {
-    const response = await Axios.get(`/globalmessage/agent`, {
-        headers : {
-          'x-access-token' : localStorage.getItem('token')
-        }
+  loadAgent = async() => {
+    const response = await Axios.get('/globalMessage/agent',{
+      headers : {
+        'x-access-token' : localStorage.getItem('token')
+      }
     })
-    console.log(response, 'agent value response')
-    this.setState({agents : response.data})
+    console.log(response, 'sers gjhgjg')
+    setAgents(response.data)
   }
-
-  handleRoomJoin = (agentId) => {
-    let roomId = agentId + this.state.clientId
-    console.log(roomId)
-    socket.emit('user-connected', roomId)
-    socket.on('messages', (data) => {console.log(data)})
-  }
-  render(){
-  
-    return(
-      <div className = "row">
-        <div className="side-paneel col-md-4">
-          
-        {
-            this.state.agents.map((agent) => {
-              return (
-                <div className = "card ml-2 p-1 text-success bg-light"
-                  onClick = {() => {this.handleRoomJoin(agent._id)}}
-                >
-                  {agent.username}
-                </div>
-              )
-            })
-          }
-        </div>
-          <div className = "chat col-md-8">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex est excepturi fugiat quod deserunt ipsa dicta illum velit perferendis. Sequi asperiores ipsam eum. Quaerat vero esse perferendis neque consequatur corrupti!</p>
-          </div>
-      </div>
-    )
-  }
+  return(
+    <div>
+      <h1>helllooo</h1>
+    </div>
+  )
 }
+
+export default Message
