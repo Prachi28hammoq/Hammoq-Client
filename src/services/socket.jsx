1012c91f5014728a33fa3d5bf7c2af8f08f9c45b
kundanchandel <kundanchandel08@gmail.com>
2020-11-19 13:58:54 +0530

    updated code

--- src/services/socket.jsx
+++ src/services/socket.jsx
@@ -1,38 +1,28 @@
 import socketIOClient from "socket.io-client";
 
-let socket = socketIOClient("https://devcust.avoidpoints.com", {transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },});
+// let socket = socketIOClient("https://devcust.avoidpoints.com", {
+//   transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
+// });
 
-if(process.env.REACT_APP_STAGE === 'devlocal')
-{
-	const socket = socketIOClient("http://localhost:8000/", 
-	{
-	  transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
-	});
+if (process.env.REACT_APP_STAGE === "devlocal") {
+  const socket = socketIOClient("http://localhost:8000/", {
+    transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
+  });
+} else if (process.env.REACT_APP_STAGE === "devhost") {
+  const socket = socketIOClient("https://devcust.avoidpoints.com", {
+    transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
+  });
+} else if (process.env.REACT_APP_STAGE === "staging") {
+  const socket = socketIOClient("https://stageapp.avoidpoints.com", {
+    transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
+  });
+} else if (process.env.REACT_APP_STAGE === "production") {
+  const socket = socketIOClient("https://app.hammoq.com", {
+    transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
+  });
 }
-
-else if(process.env.REACT_APP_STAGE === 'devhost')
-{
-	const socket = socketIOClient("https://devcust.avoidpoints.com", 
-	{
-	  transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
-	});
-}
-
-else if(process.env.REACT_APP_STAGE === 'staging')
-{
-	const socket = socketIOClient("https://stageapp.avoidpoints.com", 
-	{
-	  transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
-	});
-}
-
-else if(process.env.REACT_APP_STAGE === 'production')
-{
-	const socket = socketIOClient("https://app.hammoq.com", 
-	{
-	  transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
-	});
-}
-
+const socket = socketIOClient("http://localhost:8000/", {
+  transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
+});
 // module.exports.socket = socket;
 export default socket;
