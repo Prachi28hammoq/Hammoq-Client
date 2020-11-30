99ca493263d7236e0b65f4fc2528cb9f5b734d73
kundanchandel <kundanchandel08@gmail.com>
2020-11-30 16:30:43 +0530

    Updated code

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -3,11 +3,11 @@ import Axios from "axios";
 // const baseURL = "https://app.hammoq.com/api";
 // const assetsURL = "https://app.hammoq.com/assets/";
 
-// const baseURL = "http://stageapp.avoidpoints.com/api";
-// const assetsURL = "http://stageapp.avoidpoints.com/assets/";
+const baseURL = "http://stageapp.avoidpoints.com/api";
+const assetsURL = "http://stageapp.avoidpoints.com/assets/";
 
-const baseURL = "http://localhost:8000/api";
-const assetsURL = "http://localhost:8000/assets/";
+// const baseURL = "http://localhost:8000/api";
+// const assetsURL = "http://localhost:8000/assets/";
 
 // const baseURL = "https://devcust.avoidpoints.com/api";
 // const assetsURL = "https://devcust.avoidpoints.com/assets/";
--- src/services/socket.js
+++ src/services/socket.js
@@ -1,4 +1,4 @@
 import socketIOClient from "socket.io-client";
-export const socket = socketIOClient("http://localhost:8000", {
+export const socket = socketIOClient("http://stageapp.avoidpoints.com", {
   transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
 });
