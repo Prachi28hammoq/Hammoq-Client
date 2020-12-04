2099f50c280588615d7b03883fd41b36b5b0e505
Hammoq <67672889+Hammoq@users.noreply.github.com>
2020-12-04 22:18:57 +0530

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,4 +1,4 @@
 import socketIOClient from "socket.io-client";
-export const socket = socketIOClient("https://stageapp.avoidpoints.com", {
+export const socket = socketIOClient("https://app.hammoq.com", {
   transportOptions: { polling: { extraHeaders: { Accept: "*/*" } } },
 });
