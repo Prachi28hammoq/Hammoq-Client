dad3dc8e336af651d03591b3d488cc2d90405565
Hammoq <67672889+Hammoq@users.noreply.github.com>
2021-02-05 15:04:18 -0700

    Update PaymentComponent.jsx

--- src/Pages/Payment/PaymentComponent.jsx
+++ src/Pages/Payment/PaymentComponent.jsx
@@ -11,7 +11,7 @@ import {
 // dev- pk_test_lC5HYE8HU7h3YulsALN8XO0Y00QcNkc02w
 // live- pk_live_eHRa9qUfyNNMG2lgJVYOj9Js00hCyttVfu
 
-const stripePromise = loadStripe("pk_live_eHRa9qUfyNNMG2lgJVYOj9Js00hCyttVfu");
+const stripePromise = loadStripe("sk_test_zR2S4rLaLSBkwkqiqqgn9Tej00fvDrQ4lE");
 class PaymentComponent extends Component {
   //constructor(props) {
   //  super(props);
