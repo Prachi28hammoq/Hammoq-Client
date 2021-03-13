f2b5ed42553e032a5ceff21c282536a807f11efd
tschamp31 <tschampagne31@gmail.com>
2021-03-13 06:55:12 -0700

    Google Bucket Update.

--- src/services/Axios.jsx
+++ src/services/Axios.jsx
@@ -1,30 +1,26 @@
 import Axios from "axios";
 
 var baseURL = "";
-var assetsURL = "";
+var assetsURL = "https://storage.googleapis.com/hammoq-assets/assets/";
 
 if(process.env.REACT_APP_STAGE === 'devlocal')
 {
 	baseURL = "http://localhost:8000/api";
-	assetsURL = "http://localhost:8000/assets/";
 }
 
 else if(process.env.REACT_APP_STAGE === 'devhost')
 {
 	baseURL = "https://devapi.hammoq.com/api";
-	assetsURL = "https://devapi.hammoq.com/assets/";
 }
 
 else if(process.env.REACT_APP_STAGE === 'staging')
 {
 	baseURL = "https://stageapi.hammoq.com/api";
-	assetsURL = "https://stageapi.hammoq.com/assets/";
 }
 
 else if(process.env.REACT_APP_STAGE === 'production')
 {
 	baseURL = "https://api.hammoq.com/api";
-	assetsURL = "https://api.hammoq.com/assets/";
 }
 
 Axios.defaults.baseURL = baseURL + "/client";
